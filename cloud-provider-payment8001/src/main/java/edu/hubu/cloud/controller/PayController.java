1ccdae3c57d26cfef459cb691a7cfbe2c26dc280
workp <zaikeshishabi@163.com>
2024-07-27 16:06:34 +0800

    完整的支付模块demo构建

--- cloud-provider-payment8001/src/main/java/edu/hubu/cloud/controller/PayController.java
+++ cloud-provider-payment8001/src/main/java/edu/hubu/cloud/controller/PayController.java
@@ -2,36 +2,66 @@ package edu.hubu.cloud.controller;
 
 import edu.hubu.cloud.entities.Pay;
 import edu.hubu.cloud.entities.PayDTO;
+import edu.hubu.cloud.resp.ResultData;
+import edu.hubu.cloud.resp.ReturnCodeEnum;
 import edu.hubu.cloud.service.PayService;
 import jakarta.annotation.Resource;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class PayController{
+public class PayController
+{
     @Resource
     PayService payService;
+
     @PostMapping(value = "/pay/add")
-    public String addPay(@RequestBody Pay pay){
+    public ResultData<String> addPay(@RequestBody Pay pay)
+    {
         System.out.println(pay.toString());
         int i = payService.add(pay);
-        return "成功插入记录，返回值："+i;
+        return ResultData.success("成功插入记录，返回值："+i);
     }
+
     @DeleteMapping(value = "/pay/del/{id}")
-    public Integer deletePay(@PathVariable("id") Integer id) {
-        return payService.delete(id);
+    public ResultData<Integer> deletePay(@PathVariable("id") Integer id) {
+        int i = payService.delete(id);
+        return ResultData.success(i);
     }
+
     @PutMapping(value = "/pay/update")
-    public String updatePay(@RequestBody PayDTO payDTO){
+    public ResultData<String> updatePay(@RequestBody PayDTO payDTO)
+    {
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDTO, pay);
 
         int i = payService.update(pay);
-        return "成功修改记录，返回值："+i;
+        return ResultData.success("成功修改记录，返回值："+i);
     }
+
     @GetMapping(value = "/pay/get/{id}")
-    public Pay getById(@PathVariable("id") Integer id){
-        return payService.getById(id);
-    }//全部查询getall作为家庭作业
+    public ResultData<Pay> getById(@PathVariable("id") Integer id)
+    {
+        Pay pay = payService.getById(id);
+        return ResultData.success(pay);
+    }
+
+    @RequestMapping(value = "/pay/error",method = RequestMethod.GET)
+    public ResultData<Integer> getPayError()
+    {
+        Integer i = Integer.valueOf(200);
+        try
+        {
+            System.out.println("--------come here");
+            int data = 10/0;
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResultData.fail(ReturnCodeEnum.RC500.getCode(),e.getMessage());
+        }
+        return ResultData.success(i);
+    }
+
 }
+
+
  
